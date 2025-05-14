d32f30086be45b37e5cae41a8c9ce8269c9e14c0
Piotr BUdzyński <piotr.bud01@wp.pl>
2025-05-14 20:13:44 +0200

    Dodano pliki inicjalne do projektu

---
+++ polars_bio/quality_control.py
@@ -0,0 +1,2 @@
+# Tu powinna być implementacja naszej funkcji
+# chyba - jeszcze nie wiem dokładnie
---
+++ tbd-projekt/test.py
@@ -0,0 +1,7 @@
+import polars_bio as pb
+import polars as pl
+pl.Config(fmt_str_lengths=1000, tbl_width_chars=1000)
+pl.Config.set_tbl_cols(100)
+vcf_1 = "gs://gcp-public-data--gnomad/release/4.1/genome_sv/gnomad.v4.1.sv.sites.vcf.gz"
+result = pb.describe_vcf(vcf_1).filter(pl.col("description").str.contains(r"Latino.* allele frequency"))
+print(result)
