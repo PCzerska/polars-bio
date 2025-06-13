595e8411d05e915f6e8b2ae43d50315624f6ef3e
Piotr Budzyński <piotr.bud01@wp.pl>
2025-06-13 17:03:30 +0200

    feat: added plotting graph with sequence base content statistics

--- tbd-project/test.py
+++ tbd-project/test.py
@@ -3,7 +3,22 @@ import pandas as pd
 from polars_bio.io import read_fastq
 import polars as pl
 import time
+import matplotlib.pyplot as plt
 
+
+def display_graph(df):
+    plt.figure(figsize=(12, 6))
+    for base in ["A", "C", "G", "T", "N"]:
+        plt.plot(df["position"], df[base], label=base)
+
+    plt.xlabel("Position")
+    plt.ylabel("Base Count")
+    plt.title("Base Sequence Content Per Position")
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+    
 
 if __name__ == "__main__":
     file_name = "example3"
@@ -31,37 +46,7 @@ if __name__ == "__main__":
     ])
 
     print(flat)
-
-
-
-
-
     
-    # df = pb.sql(f"SELECT base_content_multithreaded(sequence) FROM (SELECT * FROM {file_name}) repartition BY name").collect()
-
-    # df = pb.sql(f"SELECT base_content_multithreaded(sequence) FROM {file_name}")
-    # print(df.explain(optimized=True))
-
-
-    # print(pb.sql(f"SELECT * FROM {file_name}").collect())
-    # start = time.time()
-    # # df = pb.sql(f"SELECT base_content(sequence) AS result FROM {file_name}").collect()
-    # df = pb.sql(f"SELECT base_content_multithreaded(sequence) AS result FROM {file_name}").collect()
-    # end = time.time()
-    
-    # print(f"EXECUTION TIME: {end - start}")
-
-    # # Explode the `result` list of structs into separate rows
-    # exploded = df.explode("result")
-
-    # # Convert the struct into separate columns
-    # flat = exploded.select([
-    #     pl.col("result").struct.field("position"),
-    #     pl.col("result").struct.field("A"),
-    #     pl.col("result").struct.field("C"),
-    #     pl.col("result").struct.field("G"),
-    #     pl.col("result").struct.field("T"),
-    #     pl.col("result").struct.field("N"),
-    # ])
-
-    # print(flat)
+    nested_list = df["result"][0]
+    df_converted = pd.DataFrame(nested_list, columns=["position", "A", "C", "G", "T", "N"])
+    display_graph(df_converted)
